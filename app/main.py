f5660e03bbc0591fad2249876292b8331c462045
GerardoValentino <gv.rosalesramos@ugto.mx>
2026-01-31 12:27:50 -0600

    Modifying __init__.py for easier imports

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from app.api.v1.api import api_router
+from app.api.v1 import api_router
 
-app = FastAPI(title="songs app", version="1.0.0")
+app = FastAPI(title="lyra app", version="1.0.0")
 
 app.include_router(api_router, prefix="/api/v1")
 
